f904aad779aa40dcd62fb322bf56aab175fd5edf
YousofBahieldeen <108595154+YousofBahieldeen@users.noreply.github.com>
2023-08-12 11:43:07 -0600

    take 2 making a new account

--- js/main.js
+++ js/main.js
@@ -18,12 +18,19 @@ document.getElementById("submit-1").onclick = function(){
     const usename = document.getElementById("username-1").value;
     const password = document.getElementById("password-1").value;
 
-    if (usename == "Yousof" && password == "1234" || username == "admin" && password == "12"){
-        togglePopup();
+    if (usename == "Yousof" && password == "1234"){
         signed = true;
         dowhilesined();
     }else {
         
+        document.getElementById("username-1").classList.toggle("incorrect");
+        document.getElementById("password-1").classList.toggle("incorrect");
+    }
+
+    if (username == "admin" && password == "12") {
+        signed = true;
+        dowhileadmin();
+    }else {
         document.getElementById("username-1").classList.toggle("incorrect");
         document.getElementById("password-1").classList.toggle("incorrect");
     }
@@ -34,6 +41,14 @@ document.getElementById("submit-1").onclick = function(){
 function dowhilesined() {
     if(signed == true){
         document.getElementById("signs").innerText = "Yousof";
+        document.getElementById("signs").onclick = function(){}
+        document.getElementById("games").style.visibility = 'visible';
+    }
+}
+
+function dowhileadmin(){ 
+    if(signed == true){
+        document.getElementById("signs").innerText = "Admin";
         document.getElementById("signs").onclick = function(){}
         document.getElementById("games").style.visibility = 'visible';
     }
