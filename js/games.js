e756f447bcd5cface1724f6746ef57406434cdca
YousofBahieldeen <108595154+YousofBahieldeen@users.noreply.github.com>
2023-08-12 11:53:55 -0600

    here we go :/

---
+++ js/games.js
@@ -0,0 +1,12 @@
+function togglePopup() {
+    document.getElementById("popup-1").classList.toggle("activate");
+    
+}
+
+function toggleUpdates(){
+    document.getElementById("update-1").classList.toggle("updateA");
+}
+
+function toggleNews(){
+    document.getElementById("new-1").classList.toggle("newA");
+}
